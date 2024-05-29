a60358d947fbdcdc625a91d37fa9171c4e5290be
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-05-29 16:15:09 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express") 
+const app = express();
+
+
+app.set('view engine','ejs')
+app.use(express.static('public/css'))
+
+
+app.get('/', (req,res) => {
+    res.render('home'); 
+})
+
+
+const PORT = 4100
+app.listen(PORT, ()=> {
+    console.log("Server running successfully at port " + PORT);
+}) 
+
+
+
+
+
+
